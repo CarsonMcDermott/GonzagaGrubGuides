1258ba5023fd69e8878560a0a827d17074cae426
Brady Keeley <bkeeley@zagmail.gonzaga.edu>
2023-12-04 13:28:52 -0800

    starting to work on server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.listen(3000, function() {
+    console.log("Listening on Port 3000");
+});
+
+app.get('/', function(req, res) {
+    res.render('../home.html');
+});
