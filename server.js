4f3925f7e0792476881f3a8884c6e5285cc08afd
Brady Keeley <bkeeley@zagmail.gonzaga.edu>
2023-12-04 17:39:55 -0800

    can add a new restaurant to database using the form

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const config = require('./config.json');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
 
 app.listen(3001, function() {
     console.log("Listening on Port 3001...");
@@ -17,10 +19,18 @@ app.get('/', function(req, res) {
 app.post('/newRestaurant.html', function(req, res) {
     var name = req.body.restaurantName;
     var address = req.body.address;
-    var phone = req.body.phoneNumber;
-    var type = req.body.foodType;
+    var phone_number = req.body.phoneNumber;
+    var food_type = req.body.foodType;
     var bio = req.body.bio;
     var picture = req.body.imageUpload;
     var cn = mysql.createConnection(config);
     cn.connect();
+    const q = 'INSERT INTO restaurant VALUE (?, ?, ?, ?, ?, ?)';
+    cn.query(q, [name, address, phone_number, food_type, bio, picture]);
+    // TODO: Add error Handeling for duplicates and others
+    const q2 = 'SELECT * FROM restaurant WHERE name = ?';
+    cn.query(q2, [name]);
+    res.send('<html><body><h1>Name</h1></body></html>');
+    
+    cn.end();
 });
