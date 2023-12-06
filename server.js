40ce3cc2296ecc45cc388458025f703adbc9631a
Brady Keeley <bkeeley@zagmail.gonzaga.edu>
2023-12-05 17:24:59 -0800

    add new restaurant and leave a review forms working with database

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const mysql = require('mysql2');
-// const ejs = require('ejs');
+const ejs = require('ejs');
 
 const config = require('./config.json');
 
 const app = express();
 app.set('view engine', 'ejs');
-// app.use(express.static('public'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
-// app.set('view engine', 'ejs');
-
 app.listen(3001, function() {
     console.log("Listening on Port 3001...");
 });
-
 app.get("/", function (req, res) {
-    return res.render("home.ejs");
+    return res.render("home");
+});
+app.get("/allRestaurants", function (req, res) {
+    res.render('allRestaurants');
+});
+app.get("/allReviews", function (req, res) {
+    res.render('allReviews');
 });
 
-
-
-app.get("/allRestaurants", function (req, res) {
-  return res.render("allRestaurants.ejs");
+app.get('/restaurant', function(req, res) {
+    res.render('restaurnt');
+});
+app.get('/newRestaurant', function(req, res) {
+    res.sendFile(__dirname + '/views/newRestaurant.html');
 });
 
 // app.get('/', (req, res) => {
@@ -40,43 +43,52 @@ app.get("/allRestaurants", function (req, res) {
 //     res.render('/views/pages/index', {name});
 // });
 
-// app.post('/newRestaurant.html', function(req, res) {
-//     var name = req.body.restaurantName;
-//     var address = req.body.address;
-//     var phone_number = req.body.phoneNumber;
-//     var food_type = req.body.foodType;
-//     var bio = req.body.bio;
-//     var picture = req.body.imageUpload;
-//     var cn = mysql.createConnection(config);
-//     cn.connect();
-//     const q = 'INSERT INTO restaurant VALUE (?, ?, ?, ?, ?, ?)';
-//     cn.query(q, [name, address, phone_number, food_type, bio, picture]);
-//     // TODO: Add error Handeling for duplicates and others
-//     const q2 = 'SELECT * FROM restaurant WHERE name = ?';
-//     cn.query(q2, [name]);
-//     res.send(`<html><body><h1>${name}</h1></body></html>`);
+app.post('/newRestaurant.html', function(req, res) {
+    var name = req.body.restaurantName;
+    var address = req.body.address;
+    var phone_number = req.body.phoneNumber;
+    var food_type = req.body.foodType;
+    var bio = req.body.bio;
+    var picture = req.body.imageUpload;
+    var cn = mysql.createConnection(config);
+    cn.connect();
+    const q = 'INSERT INTO restaurant VALUE (?, ?, ?, ?, ?, ?)';
+    cn.query(q, [name, address, phone_number, food_type, bio, picture], function(err) {
+        if (err) {console.log('Error: ', err);}
+    });
+    const q2 = 'SELECT * FROM restaurant WHERE name = ?';
+    cn.query(q2, [name], function(err, rows, fields) {
+        res.render('restaurant', {
+            name : rows[0].name,
+            address : rows[0].address,
+            phone_number : rows[0].phone_number,
+            food_type : rows[0].food_type,
+            bio : rows[0].bio
+        });
+    });
     
-//     cn.end();
-// });
 
-// app.post('/review.html', function(req, res) {
-//     var name = req.body.restaurantName;
-//     var address = req.body.address;
-//     var phone_number = req.body.phoneNumber;
-//     var food_type = req.body.foodType;
-//     var bio = req.body.bio;
-//     var picture = req.body.imageUpload;
-//     var cn = mysql.createConnection(config);
-//     cn.connect();
-//     const q = 'INSERT INTO restaurant VALUE (?, ?, ?, ?, ?, ?)';
-//     cn.query(q, [name, address, phone_number, food_type, bio, picture]);
-//     // TODO: Add error Handeling for duplicates and others
-//     const q2 = 'SELECT * FROM restaurant WHERE name = ?';
-//     cn.query(q2, [name]);
-//     res.send(`<html><body><h1>${name}</h1></body></html>`);
-    
-//     cn.end();
-// });
+    cn.end();
+});
+
+app.post('/review.html', function(req, res) {
+    var name = req.body.restaurantName;
+    var rating = req.body.radio;
+    var comments = req.body.comments;
+    var picture = req.body.imageUpload;
+    var cn = mysql.createConnection(config);
+    cn.connect();
+    const q = 'INSERT INTO review (name, rating, comments, picture) VALUE (?, ?, ?, ?)';
+    cn.query(q, [name, rating, comments, picture], function(err, rows, fields) {
+        if (err) {console.log('Error: ', err);}
+    });
+    const q2 = 'SELECT * FROM review WHERE name = ?';
+    cn.query(q2, [name], function(err, rows, fields) {
+        if (err) {console.log('Error: ', err);}
+        res.render('allReviews');
+    });
+    cn.end();
+});
 
 app.get('/review', function(req, res) {
     var cn = mysql.createConnection(config);
