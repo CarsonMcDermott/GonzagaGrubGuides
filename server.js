8be68ba3a1598a644a8d2430591061b8e1d75cab
Brady Keeley <bkeeley@zagmail.gonzaga.edu>
2023-12-14 21:01:58 -0800

    working with pictures

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const config = require('./config.json');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(fileupload());
